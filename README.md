7a5a3c2d13a665c03f3875c250c01318853ddf26
junghs1040 <junghs1040@naver.com>
2021-07-11 16:12:52 +0900

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # 1leg_test
  This project is 1 leg structure of Quadruped robot project to test trajectory generation, control the robot
+
+![Screenshot from 2021-07-05 10-44-24](https://user-images.githubusercontent.com/19335771/125185978-aeaa4980-e262-11eb-9e93-dbffbe5cc8dd.png)
---
+++ leg1_project_libs/src/robot_node.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+#include "ros/ros.h"
+#include "sensor_msgs/JointState.h"
+#include "tf/transform_broadcaster.h"
+#include "urdf/model.h"
+#include "leg1_project_libs/robot_node.hpp"
+
+int main(int argc, char** argv)
+{
+
+    return 0;
+}
